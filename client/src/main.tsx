47f83b8d81f9c52b366c1ac961eb7ab33dbc598c
Cristian <cdom127@wgu.edu>
2025-03-09 21:18:25 -0700

    Update assets

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <main>
+      <p className='font-r-t text-4xl'>Thin R Font</p>
+      <p className='font-r-ul text-4xl'>UltraLight R Font</p>
+      <p className='font-r-ul-i text-4xl'>UltraLight Italic R Font</p>
+      <p className='font-s-r text-4xl'>Regular S Font</p>
+      <p className='font-s-m text-4xl'>Medium S Font</p>
+    </main>
+  </StrictMode>
+);
